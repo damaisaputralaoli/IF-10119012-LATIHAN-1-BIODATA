586669aba2d718adfd5f97db8cbcd56d5c72d105
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-10-20 21:26:42 +0700

    Add files via upload

---
+++ Latihan1Pbo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1.pbo;
+
+/**
+ *
+ * @author damai
+ */
+public class Latihan1Pbo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi variabel
+      System.out.println("BIODATA ROCKSTAR"+ "\n" + "--------------------");
+        System.out.println("NAMA\t\t: Damai Saputra laoli");
+        System.out.println("ALAMAT\t\t: Karawang");
+        System.out.println("TEMPAT LAHIR\t: Jakarta");
+        System.out.println("TANGGAL LAHIR\t: 15 July 1999");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: Futsal & Renang");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: ES CAMPUR");
+    }
+    
+}
